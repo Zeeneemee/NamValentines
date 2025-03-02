ea72d742ed51dba60ffdb40967fe88e71f8b8fa8
Tt <tt.taiton@gmail.com>
2025-03-03 06:07:47 +0700

    change router

--- love/src/App.js
+++ love/src/App.js
@@ -1,18 +1,17 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Year1 from './pages/year1';
 import Year2 from './pages/year2';
 import './index.css'
 import MemoryLog from './pages/memoryLog';
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/year1" />} />
-        <Route path="/year1" element={<Year1 />} />
-        <Route path="/year2" element={<Year2 />} />
-        <Route path="/memory-log" element={<MemoryLog/>} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Year2 />} />
+      <Route path="/year2" element={<Year2 />} />
+      <Route path="/memory-log" element={<MemoryLog />} />
+    </Routes>
+  </Router>
   );
 }
 
